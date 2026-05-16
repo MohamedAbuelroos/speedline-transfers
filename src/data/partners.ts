30a7d64ae11f0c77b7d5af4df167a58528f0ddf4
MohamedRabea <mohamedabuelroos27@gmail.com>
2026-05-16 13:42:26 +0300

    partner swiper

---
+++ src/data/partners.ts
@@ -0,0 +1,51 @@
+// import partner1 from "../assets/partners/partner1.png";
+// import partner2 from "../assets/partners/partner2.png";
+// import partner3 from "../assets/partners/partner3.png";
+// import partner4 from "../assets/partners/partner4.png";
+// import partner5 from "../assets/partners/partner5.png";
+
+export const partners = [
+  {
+    id: 1,
+    image: "https://static.mozio.com/assets/logo.svg",
+    name: "MOZIO",
+  },
+
+  {
+    id: 2,
+    image:
+      "https://elifelimo.com/_next/image?url=https%3A%2F%2Fcms.elifelimo.com%2Fuploads%2Flogo_02_1fa3d696ca.png&w=128&q=75",
+    name: "ELIFE LIMO",
+  },
+
+  {
+    id: 3,
+    image: "https://portal.mistertransfer.com/logo/mainlogo.svg",
+    name: "MISTER TRANSFER",
+  },
+
+  {
+    id: 4,
+    image:
+      "https://cms.hoppa.com/uploads/hoppa_logo_no_strapline_2_1_94dc1f946c.svg?v=1776252521835",
+    name: "HOPPA",
+  },
+
+  {
+    id: 5,
+    image:
+      "https://egypttravelist.com/wp-content/uploads/2023/02/1-1-e1694183823248-600x334.png.webp",
+    name: "EGYPT TRAVELIST",
+  },
+  {
+    id: 6,
+    image: "https://gettransfer.com/common/logo.svg?v=2026.1",
+    name: "GET TRANSFER",
+  },
+  {
+    id: 7,
+    image:
+      "https://cdn.prod.website-files.com/643ce6c7e402d441e1c0bfc7/643e056a052343fe2bffd33b_Logo-color.svg",
+    name: "BLACKLANE",
+  },
+];
---
+++ src/pages/Home/Sections/PartnersSection.tsx
@@ -0,0 +1,155 @@
+import { Box, Container, Typography } from "@mui/material";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { Autoplay } from "swiper/modules";
+
+import "swiper/css";
+
+import { partners } from "../../../data/partners";
+
+const PartnersSection = () => {
+  return (
+    <Box
+      sx={{
+        py: {
+          xs: 7,
+          md: 9,
+        },
+
+        background: `
+        linear-gradient(
+          180deg,
+          #ffffff 0%,
+          #f8fbff 100%
+        )
+      `,
+      }}
+    >
+      <Container maxWidth="lg">
+        {/* HEADER */}
+        <Box
+          sx={{
+            textAlign: "center",
+
+            mb: 5,
+          }}
+        >
+          <Typography
+            sx={{
+              fontWeight: 800,
+
+              color: "#111827",
+
+              mb: 1,
+
+              fontSize: {
+                xs: 26,
+                md: 34,
+              },
+            }}
+          >
+            Trusted By Leading Partners
+          </Typography>
+
+          <Typography
+            sx={{
+              color: "#6b7280",
+
+              maxWidth: 650,
+
+              mx: "auto",
+
+              lineHeight: 1.9,
+            }}
+          >
+            We proudly collaborate with hotels, travel agencies, tourism
+            companies, and corporate partners across Saudi Arabia.
+          </Typography>
+        </Box>
+
+        {/* SWIPER */}
+        <Swiper
+          modules={[Autoplay]}
+          slidesPerView={2}
+          spaceBetween={20}
+          loop
+          autoplay={{
+            delay: 0,
+            disableOnInteraction: false,
+            stopOnLastSlide: false,
+
+            pauseOnMouseEnter: true,
+          }}
+          speed={4000}
+          breakpoints={{
+            640: {
+              slidesPerView: 3,
+            },
+
+            900: {
+              slidesPerView: 4,
+            },
+
+            1200: {
+              slidesPerView: 5,
+            },
+          }}
+        >
+          {partners.map((partner) => (
+            <SwiperSlide key={partner.id}>
+              <Box
+                sx={{
+                  height: 120,
+
+                  borderRadius: "28px",
+
+                  backgroundColor: "#fff",
+
+                  border: "1px solid rgba(226,232,240,0.7)",
+
+                  display: "flex",
+
+                  alignItems: "center",
+
+                  justifyContent: "center",
+
+                  transition: "0.35s ease",
+
+                  opacity: 0.7,
+
+                  filter: "grayscale(100%)",
+
+                  "&:hover": {
+                    opacity: 1,
+
+                    filter: "grayscale(0%)",
+
+                    transform: "translateY(-4px)",
+
+                    boxShadow: "0 15px 35px rgba(15,23,42,0.08)",
+                  },
+                }}
+              >
+                <Box
+                  component="img"
+                  src={partner.image}
+                  alt={partner.name}
+                  sx={{
+                    width: "70%",
+
+                    height: "70%",
+
+                    objectFit: "contain",
+                  }}
+                />
+              </Box>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Container>
+    </Box>
+  );
+};
+
+export default PartnersSection;
